efb374c09cdeff6535eaac08ae9aaf695c18914a
Eliass-TROY <troyeliasssaturnin@gmail.com>
2026-08-31 00:05:17 +0300

    Modification du projet et ajout du dossier saisinen

--- script.js
+++ script.js
@@ -11,3 +11,106 @@ document.addEventListener("click", function (event) {
         sidebar.classList.remove("show");
     }
 });
+
+// saisine/saisine.js
+
+const searchInput = document.getElementById("searchInput");
+const statusFilter = document.getElementById("statusFilter");
+const gravityFilter = document.getElementById("gravityFilter");
+
+const table = document.getElementById("saisinesTable");
+const resultCount = document.getElementById("resultCount");
+
+
+function filterSaisines() {
+
+    const search = searchInput.value.toLowerCase();
+    const status = statusFilter.value;
+    const gravity = gravityFilter.value;
+    const rows = table.querySelectorAll("tr");
+    let count = 0;
+
+    rows.forEach(row => {
+        const text = row.textContent.toLowerCase();
+        const statusText =
+            row.children[5].textContent.trim();
+
+        const gravityText =
+            row.children[4].textContent.trim();
+
+
+        const searchMatch =
+            text.includes(search);
+
+
+        const statusMatch =
+            status === "all" ||
+            statusText === status;
+
+
+        const gravityMatch =
+            gravity === "all" ||
+            gravityText === gravity;
+
+
+        if (
+            searchMatch &&
+            statusMatch &&
+            gravityMatch
+        ) {
+
+            row.style.display = "";
+
+            count++;
+
+        } else {
+
+            row.style.display = "none";
+
+        }
+
+    });
+
+
+    resultCount.textContent =
+        count + " saisine(s) trouvée(s)";
+}
+
+
+searchInput.addEventListener(
+    "input",
+    filterSaisines
+);
+
+
+statusFilter.addEventListener(
+    "change",
+    filterSaisines
+);
+
+
+gravityFilter.addEventListener(
+    "change",
+    filterSaisines
+);
+
+
+/* ACTION DE TRAITEMENT */
+
+document.querySelectorAll(".view-btn").forEach(button => {
+
+    button.addEventListener("click", function () {
+
+        const row = this.closest("tr");
+
+        const reference =
+            row.children[0].textContent;
+
+        alert(
+            "Ouverture du traitement de : " +
+            reference
+        );
+
+    });
+
+});
